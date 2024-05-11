0a1ed9463ddee765168cd10e17c052ef54ee0a84
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-05-11 18:43:54 +0800

    books by tag fetched

--- app/(guest)/tags/page.tsx
+++ app/(guest)/tags/page.tsx
@@ -3,7 +3,6 @@
 
 import React, { useState, useEffect, ChangeEvent, FormEvent, useCallback, useRef } from 'react';
 import axios, { AxiosResponse } from 'axios';
-import { Author, Book } from './types';
 import ReactPaginate from 'react-paginate';
 import AuthorTag from '@/components/books/author-tag';
 import SubjectTag from '@/components/books/subject-tag';
@@ -23,56 +22,78 @@ import BreadcrumbComponent from '@/components/breadcrumb';
 import CartSheet from '@/components/cart-sheet';
 import Link from 'next/link';
 import NavMenu from '@/components/nav-menu';
+import PaginationControls from '@/components/pagination-controls';
 
 const appUrl = process.env.NEXT_PUBLIC_APP;
 
-const AuthorsPage: React.FC = () => {
+interface Subject {
+  id: number;
+  subject_name: string;
+  temp_id?: number | null;
+  subject_code: string;
+  books: Book[];
+}
+
+interface Book {
+  id: number;
+  title: string;
+}
+
+const TagPage: React.FC = () => {
+  const [subjects, setSubjects] = useState<Subject[]>([]);
+  const [pageCount, setPageCount] = useState<number>(0);
   const [totalPages, setTotalPages] = useState<number>(1);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [authors, setAuthors] = useState<Author[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [inputPage, setInputPage] = useState<string>(''); 
-  const [bookTitlesCount, setBookTitlesCount] = useState<number>(0);
   const [bookTitles, setBookTitles] = useState<string[]>([]);
-
-  
-  useEffect(() => {
-    fetchAuthors(currentPage);
-  }, [currentPage]);
+  const [bookTitlesCount, setBookTitlesCount] = useState<number>(0);
 
   useEffect(() => {
-    const storedTitlesJSON = localStorage.getItem('bookTitles');
-    if (storedTitlesJSON) {
-      setBookTitles(JSON.parse(storedTitlesJSON));
-    }
+    fetchSubjects(currentPage); // Fetch first page of subjects on component mount
+  }, [currentPage]); // Watch for changes in currentPage
 
-    const storedCount = localStorage.getItem('bookTitlesCount');
-    if (storedCount) {
-      setBookTitlesCount(parseInt(storedCount, 10));
-    }
-  }, []);
-
-  const fetchAuthors = async (page: number): Promise<void> => {
+  const fetchSubjects = async (page: number): Promise<void> => {
     setLoading(true);
     try {
-      const response = await axios.get(`${appUrl}api/authors/?page=${page}`);
+      const response = await axios.get(`${appUrl}api/subjects/?page=${page}`);
       const { count, results } = response.data;
-
-      const totalPagesCount: number = Math.ceil(count / 10);
-      setAuthors(results);
-      setTotalPages(totalPagesCount);
+  
+      const updatedResults = results.map((subject: Subject) => {
+        if (!subject.books) {
+          subject.books = [];
+        }
+        return subject;
+      });
+  
+      setSubjects(updatedResults);
+      setTotalPages(Math.ceil(count / 10));
       setLoading(false);
     } catch (error) {
-      console.error('Error fetching authors:', error);
+      console.error('Error fetching subjects:', error);
       setLoading(false);
-    } finally {
-      setLoading(false); // Always set loading state to false after fetching
     }
   };
-  
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setInputPage(e.target.value); // Update inputPage state with user input
+  const fetchBooksForSubject = async (subjectId: number): Promise<Book[]> => {
+    try {
+      const response = await axios.get(`${appUrl}api/subjects/${subjectId}/`);
+      return response.data.results;
+    } catch (error) {
+      console.error(`Error fetching books for subject ${subjectId}:`, error);
+      return [];
+    }
+  };
+
+  
+  const handlePageClick = (data: { selected: number }): void => {
+    setLoading(true);
+    setCurrentPage(data.selected + 1);
+  };
+
+  const handlePageChange = (selectedPage: number) => {
+    setCurrentPage(selectedPage);
+    fetchSubjects(selectedPage); // Fetch subjects for the selected page
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
@@ -84,12 +105,45 @@ const AuthorsPage: React.FC = () => {
     setInputPage(''); // Clear input field after submission
   };
 
-
-  const handlePageClick = (data: { selected: number }): void => {
-    setLoading(true);
-    setCurrentPage(data.selected + 1);
+  
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputPage(e.target.value); // Update inputPage state with user input
   };
 
+
+  const renderSubjects = () => {
+    return (
+      <ul>
+        {subjects.map((subject: Subject, index: number) => (
+          <li key={subject.id || `subject-${index}`}>
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem value={`item-${subject.id}`}>
+                <AccordionTrigger>
+                <span className="opacity-90 bg-purple-100 text-purple-500 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-500">{subject.subject_name} </span>
+                <span className="text-yellow-200"> {subject.books.length} ( book tagged)</span>
+                </AccordionTrigger>
+                <ul>
+                  {subject.books ? (
+                    subject.books.map((book: Book, index: number) => (
+                      <li key={book.id || `book-${index}`}>
+                        <AccordionContent>{book.title}</AccordionContent>
+                      </li>
+                    ))
+                  ) : (
+                    <li key={`no-books-${subject.id}`}>No books found for this subject</li>
+                  )}
+                </ul>
+              </AccordionItem>
+            </Accordion>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+  
+
+
+  
   const handleDeleteBookTitle = (titleToRemove: string) => {
     const updatedTitles = bookTitles.filter((title) => title !== titleToRemove);
     localStorage.setItem('bookTitles', JSON.stringify(updatedTitles));
@@ -97,6 +151,7 @@ const AuthorsPage: React.FC = () => {
     setBookTitlesCount(updatedTitles.length);
   };
 
+  
   const handleEmptyBookCart = () => {
     setBookTitles([]);
     setBookTitlesCount(0);
@@ -112,7 +167,7 @@ const AuthorsPage: React.FC = () => {
       <div className="col-span-3 lg:col-span-4 lg:border-l">
       <div className="h-full px-4 py-6 lg:px-8">
       <div className='flex justify-between'>
-          <BreadcrumbComponent currentPage={currentPage} currentPageText="Authors" />
+          <BreadcrumbComponent currentPage={currentPage} currentPageText="Tags" />
           <div className='mr-16'>
             <CartSheet bookTitles={bookTitles} onDeleteTitle={handleDeleteBookTitle} handleEmptyBookCart={handleEmptyBookCart} />
           </div>
@@ -120,32 +175,15 @@ const AuthorsPage: React.FC = () => {
       <div className='mb-2'>
         <NavMenu />
       </div>
-      <h2 className="text-2xl font-bold mb-4">AuthorsPage <span className="bg-purple-100 text-purple-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-400">{currentPage}</span></h2>
+      <h2 className="text-2xl font-bold mb-4">All Tags <span className="bg-purple-100 text-purple-800 text-lg font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-purple-400 border border-purple-400">{currentPage}</span></h2>
       {loading ? ( 
         <div className='h-full'>
           <FidgetSpinner />
         </div>
       ) : (
-      <ul>
-        {authors.map((author: Author) => (
-          <li key={author.id}>
-            <Accordion type="single" collapsible className="w-full">
-              <AccordionItem value={`item-${author.id}`}>
-                <AccordionTrigger>
-                  {author.author_name} <span className="text-yellow-200"> {author.books.length} ( book published)</span>
-                </AccordionTrigger>
-                <ul>
-                  {author.books.map((book: Book) => (
-                    <li key={book.id}>
-                      <AccordionContent>{book.title}</AccordionContent>
-                    </li>
-                  ))}
-                </ul>
-              </AccordionItem>
-            </Accordion>
-          </li>
-        ))}
-      </ul>
+        <div>
+        {renderSubjects()}
+        </div>
           )}
       {/* Pagination */}
       <div className="mt-4 flex justify-left items-center">
@@ -194,4 +232,4 @@ const AuthorsPage: React.FC = () => {
   );
 };
 
-export default AuthorsPage;
+export default TagPage;
